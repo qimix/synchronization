89b8a0c6c22bc2a7ea19a237938ff27dcfcefe51
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-05 02:16:48 +0500

    init commit

---
+++ src/main/java/ru/qimix/Main.java
@@ -0,0 +1,41 @@
+package ru.qimix;
+
+import java.util.*;
+
+public class Main {
+    public static Map<Integer, Integer> sizeToFreq = Collections.synchronizedMap(new HashMap<>());
+    public static List<String> routeStore = Collections.synchronizedList(new ArrayList<>());
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 1000; i++) {
+            new Robot("RLRFR", 100).start();
+        }
+    }
+
+    static class Robot extends Thread {
+        String letters;
+        Integer length;
+
+        public Robot(String letters, int length){
+            this.letters = letters;
+            this.length = length;
+        }
+
+        public void run(){
+            generateRoute(letters, length);
+        }
+
+        public String generateRoute(String letters, int length) {
+            Random random = new Random();
+            StringBuilder route = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                route.append(letters.charAt(random.nextInt(letters.length())));
+            }
+            synchronized (routeStore) {
+                routeStore.add(route.toString());
+            }
+            return route.toString();
+        }
+
+    }
+}
